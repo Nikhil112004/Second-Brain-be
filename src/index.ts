ebf1e3116bc69bf3a575d5bfd138db04bc1f57fd
Nikhil Gupta <164629304+Nikhil112004@users.noreply.github.com>
2025-08-06 11:55:50 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { JWT_PASSWORD } from "./config";
 import { userMiddleware } from "./middleware";
 import { random } from "./utils";
 import cors from "cors";
+import { NowRequest, NowResponse } from '@vercel/node'
 
 const app = express();
 app.use(express.json());
